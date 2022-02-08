7f6924d8603087e897d84cf082e454fbebbd00f3
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-02-08 11:50:31 +0600

    10 paragraphs

--- script.js
+++ script.js
@@ -24,42 +24,63 @@
 // document.body.textContent = "Hello world";
 
 // getElementById
-let helloWorldDiv = document.getElementById("hello-world");
+// let helloWorldDiv = document.getElementById("hello-world");
 
-helloWorldDiv.textContent = "Nice to meet you!";
-helloWorldDiv.style.background = "green";
-helloWorldDiv.style.color = "white";
-helloWorldDiv.style.textDecoration = "underline";
-helloWorldDiv.style.fontSize = "28px";
+// helloWorldDiv.textContent = "Nice to meet you!";
+// helloWorldDiv.style.background = "green";
+// helloWorldDiv.style.color = "white";
+// helloWorldDiv.style.textDecoration = "underline";
+// helloWorldDiv.style.fontSize = "28px";
 
-// getElementsByTagName
-let allParagraphs = document.getElementsByTagName("p");
+// // getElementsByTagName
+// let allParagraphs = document.getElementsByTagName("p");
 
-for(let paragraph of allParagraphs) {
-    paragraph.style.backgroundColor = "blue";
+// for(let paragraph of allParagraphs) {
+//     paragraph.style.backgroundColor = "blue";
 
-}
-let allHelloWorld = document.getElementsByClassName("hello-world");
+// }
+// let allHelloWorld = document.getElementsByClassName("hello-world");
 
-for(let helloWorld of allHelloWorld) {
-    helloWorld.style.color = "red";
-    helloWorld.style.backgroundColor = "yellow";
-    helloWorld.style.fontStyle = "italic";
+// for(let helloWorld of allHelloWorld) {
+//     helloWorld.style.color = "red";
+//     helloWorld.style.backgroundColor = "yellow";
+//     helloWorld.style.fontStyle = "italic";
 
-}
+// }
 
 // querySelector
-let helloWorld2 = document.querySelector("#hello-world");
-helloWorld2.style.backgroundColor = "purple";
+// let helloWorld2 = document.querySelector("#hello-world");
+// helloWorld2.style.backgroundColor = "purple";
 
-let allHelloWorlds2 = document.querySelectorAll(".hello-world");
+// let allHelloWorlds2 = document.querySelectorAll(".hello-world");
 
-for(let helloWorld of allHelloWorld) {
-    helloWorld.style.color = "red";
-    helloWorld.style.backgroundColor = "yellow";
-    helloWorld.style.fontStyle = "italic";
+// for(let helloWorld of allHelloWorld) {
+//     helloWorld.style.color = "red";
+//     helloWorld.style.backgroundColor = "yellow";
+//     helloWorld.style.fontStyle = "italic";
 
+// }
+document.querySelectorAA('p');
+for (let p of allP) {
+    p.style.flexBasis = "90%"
 }
+var button1 = document.querySelector('button:nth-of-type(1)');
+var button2 = document.querySelector('button:nth-of-type(2)');
+var button3 = document.querySelector('button:nth-of-type(3)');
 
-
-
+button1.aadEventListener('click', function() {
+    for (let p of allP) {
+        p.style.flexBasis = "90%"
+}
+});
+    button2.aadEventListener('click', function() {
+        for (let p of allP) {
+            p.style.flexBasis = "45%"
+    }
+});
+        button3.aadEventListener('click', function() {
+            for (let p of allP) {
+                p.style.flexBasis = "30%"
+        }
+    });
+            
